b198b370c9c16063dc4c43e54c2401f56272dec6
crisenyel <ccanas.h@gmail.com>
2024-12-29 20:00:34 -0300

    testing only

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 runFrontend rf:
 	ng serve shell -o; ng serve mfe1 -o
 
+sync:
+	npx cap sync
+
 buildLibrary bl:
 	
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lib.app',
+  appName: 'lib-to-apk',
+  // webDir: 'dist'
+  webDir: 'dist/shell'
+};
+
+export default config;
